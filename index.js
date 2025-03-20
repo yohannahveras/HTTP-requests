8b35a65457c6409750754647a04c1b40dcadb621
Yohannah Véras <yohannahlv@gmail.com>
2025-03-20 11:00:05 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('<h1>Homepage</h1>')
+})
+
+app.get('/about', (req, res) => {
+  res.send('<h1>About Me</h1>')
+})
+
+app.get('/contact', (req, res) => {
+  res.send('<h1>Contact</h1>')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
